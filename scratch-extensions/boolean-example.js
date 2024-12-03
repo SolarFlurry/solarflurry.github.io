e4922ae6e0ffc30d179664745561f5565d0972f0
SolarFlurry <sethslfung@gmail.com>
2024-12-03 16:20:57 +1100

    FetchExtension made

--- scratch-extensions/boolean-example.js
+++ scratch-extensions/boolean-example.js
@@ -1,30 +1,60 @@
-class BooleanExample {
+class FetchExtension {
     constructor() {
     }
-    
+
     getInfo() {
         return {
-            "id": "Booleans",
-            "name": "Standard Booleans",
-            "blocks": [ {
-                            "opcode": "rtrue",
-                            "blockType": "Boolean",
-                            "text": "true",
+            "id": "Fetch",
+            "name": "Fetch",
+            "blocks": [
+                        {
+                            "opcode": "fetchURL",
+                            "blockType": "reporter",
+                            "text": "fetch data from [url]",
+                            "arguments": {
+                                "url": {
+                                    "type": "string",
+                                    "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
+                                },
                             }
-                        },{"opcode": "rfalse",
-                            "blockType": "Boolean",
-                            "text": "false",
+                        },
+                        {
+                            "opcode": "jsonExtract",
+                            "blockType": "reporter",
+                            "text": "extract [name] from [data]",
+                            "arguments": {
+                                "name": {
+                                    "type": "string",
+                                    "defaultValue": "temperature"
+                                },
+                                "data": {
+                                    "type": "string",
+                                    "defaultValue": '{"temperature": 12.3}'
+                                },
                             }
-                        }, ]
+                        },
+                ]
         }
     }
 
-    rtrue(){
-		return true
-	}
-	rfalse(){
-		return false
-	}
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
+    }
+    fetchURL({url}) {
+        return fetch(url).then(response => response.text())
+    }
 }
 
-Scratch.extensions.register(new BooleanExample())
+Scratch.extensions.register(new FetchExtension())
