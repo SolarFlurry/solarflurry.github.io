1ad63007930491d4c6f83e501d8b6851f7ad7a8c
SolarFlurry <92278004+SolarFlurry@users.noreply.github.com>
2026-02-16 19:31:33 +1100

    improved starfield

--- js/index.js
+++ js/index.js
@@ -83,18 +83,23 @@ ctx.lineWidth = 2;
 ctx.lineCap = "round";
 
 let accelerating = 0;
-let time = 0;
-const STREAK = 8;
+let accelerateStart = 0;
+let lastTime;
+const STREAK = 2;
 
-const ACCELERATE_SPEED = 50;
-const MAX_SPEED = 0.01;
-const MIN_SPEED = 0.001;
+const ACCELERATE_SPEED = 60;
+const MAX_SPEED = 0.002;
+const MIN_SPEED = 0.0002;
 let speed = MIN_SPEED;
 
-const points = [];
+let paused = false;
+
+const STAR_COUNT = 1000;
+
+const points = new Float32Array(STAR_COUNT * 3);
 
 function accelerate(cond) {
-	time = 0;
+	accelerateStart = performance.now();
 	if (cond) {
 		accelerating = 1
 	} else {
@@ -102,45 +107,80 @@ function accelerate(cond) {
 	}
 }
 
-function randomPoint() {
-	return {
-		x: Math.random() * canvas.width * 2 - canvas.width / 2,
-		y: Math.random() * canvas.height * 2 - canvas.height / 2,
-		z: Math.random() * 2 + 0.1,
+function setRandomPoint(i) {
+	points[i] = Math.random() * canvas.width * 2 - canvas.width / 2
+	points[i + 1] = Math.random() * canvas.height * 2 - canvas.height / 2
+	points[i + 2] = Math.random() * 2 + 0.1
+}
+
+for (let i = 0; i < STAR_COUNT * 3; i += 3) {
+	setRandomPoint(i);
+}
+
+window.addEventListener("resize", () => {
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+});
+
+document.addEventListener('visibilitychange', () => {
+	paused = document.hidden;
+	lastTime = performance.now();
+})
+
+const frame = (timestamp) => {
+	if (paused) {
+		window.requestAnimationFrame(frame);
+		return;
 	}
-}
 
-for (let i = 0; i < 1000; i++) {
-	points.push(randomPoint());
-}
+	let delta;
+	if (lastTime == undefined) {
+		delta = 1;
+	} else {
+		delta = timestamp - lastTime;
+	}
+	let elapsed = timestamp - accelerateStart;
 
-setInterval(() => {
-	canvas.height = window.innerHeight + 20;
-	canvas.width = window.innerWidth + 20;
 	if (accelerating == 1) {
-		speed = (Math.sin(time / ACCELERATE_SPEED - Math.PI / 2) * 0.5 + 0.5) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED
-		if (time >= Math.PI * ACCELERATE_SPEED) { speed = MAX_SPEED; }
+		speed = (Math.sin(elapsed / ACCELERATE_SPEED - Math.PI / 2) * 0.5 + 0.5) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED
+		if (elapsed >= Math.PI * ACCELERATE_SPEED) { speed = MAX_SPEED; }
 	} else if (accelerating == -1) {
-		speed = (0.5 - Math.sin(time / ACCELERATE_SPEED - Math.PI / 2) * 0.5) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED
-		if (time >= Math.PI * ACCELERATE_SPEED) { accelerating = 0; }
+		speed = (0.5 - Math.sin(elapsed / ACCELERATE_SPEED - Math.PI / 2) * 0.5) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED
+		if (elapsed >= Math.PI * ACCELERATE_SPEED) { accelerating = 0; }
 	} else {
 		speed = MIN_SPEED;
 	}
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	for (const point of points) {
-		ctx.lineWidth = Math.max(1 / point.z, 0.1);
-		ctx.strokeStyle = `rgba(255, 255, 255, ${Math.min(1.9 - point.z, 1)}`
+
+	ctx.globalCompositeOperation = 'destination-out';
+	ctx.fillStyle = `rgba(0, 0, 0, ${0.1 * (1 - speed / MAX_SPEED) + 0.5})`;
+  	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.globalCompositeOperation = 'source-over'
+
+	const cx = canvas.width / 2;
+	const cy = canvas.height / 2;
+
+	for (let i = 0; i < STAR_COUNT * 3; i += 3) {
 		ctx.beginPath();
-		ctx.moveTo((point.x - canvas.width / 2) / point.z + canvas.width / 2, (point.y - canvas.height / 2) / point.z + canvas.height / 2);
-		ctx.lineTo((point.x - canvas.width / 2) / (point.z + speed * STREAK) + canvas.width / 2, (point.y - canvas.height / 2) / (point.z + speed * STREAK) + canvas.height / 2);
+		ctx.lineWidth = Math.max(1 / points[i + 2], 0.1);
+		ctx.strokeStyle = `rgba(255, 255, 255, ${Math.min(1.9 - points[i + 2], 1)}`
+		ctx.moveTo(
+			(points[i] - cx) / points[i + 2] + cx,
+			(points[i + 1] - cy) / points[i + 2] + cy,
+		);
+		ctx.lineTo(
+			(points[i] - cx) / (points[i + 2] + speed * delta * STREAK) + cx,
+			(points[i + 1] - cy) / (points[i + 2] + speed * delta * STREAK) + cy,
+		);
 		ctx.stroke();
-		point.z -= speed;
-		if (point.z <= speed) {
-			let newPoint = randomPoint();
-			point.x = newPoint.x;
-			point.y = newPoint.y;
-			point.z = 2;
+		points[i + 2] -= speed * delta;
+		if (points[i + 2] <= speed * delta) {
+			setRandomPoint(i);
+			points[i + 2] = 2;
 		}
 	}
-	time += 1;
-}, 0)
+
+	lastTime = timestamp;
+	window.requestAnimationFrame(frame);
+}
+
+window.requestAnimationFrame(frame);
